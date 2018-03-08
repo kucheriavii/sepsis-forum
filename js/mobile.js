60b5e3e13a976a514171ab590e78000c2d85d3e9
kucheriavii <kucheriavii@gmail.com>
2018-03-08 15:59:37 +0200

    partners

--- js/mobile.js
+++ js/mobile.js
@@ -4,9 +4,20 @@
 $(function(){
     detectWidth();
     mainNewsAdaptive(); /*Делаю с двух колонок единую*/
+    mainPartnersAdaptive(); /*Порядок в партнерах*/
     $(window).resize(detectWidth); /*если таблет вешаю id=tablet*/
-    //сюда запилить функцию которая будет перезагружать страницу, если меняется вид (таблетка/мобильник)
+
     $(".events-list").mCustomScrollbar('destroy');
+    $(".events-block_wrap").mCustomScrollbar('destroy');
+
+    tabletSliders(); //пересобираю слайдеры для адаптивной верстки
+
+    /*Перезагрузка страници при изменении вида*/
+    startWindowsWidth = $(window).width();
+    $(window).resize(reloadWhenChangeViews);
+
+
+    console.log($('.partners-section_wrap .partners-block_wrap img'))
 });
 
 
@@ -41,21 +52,91 @@ var detectWidth = function () {
 поскольку в изначальной реализации подразумевается два блока, а в мобильной версии один*/
 var mainNewsAdaptive = function () {
   if(isDisplayTablet()){
-      $('.right-side .news-block').addClass('js-tabletInputedItems').insertAfter('.left-side .news-block');
-      $('.right-side').remove();
-      $('.left-side .slider-block').insertAfter('.left-side'); //выдераем банер-слайдер с новостной колонки и вставляем его после блока. Потому что новости будут иметь свой слайдер
-      $('.left-side .slider-block').remove();
+      $('.news-block_wrap .right-side .news-block').addClass('js-tabletInputedItems').insertAfter('.left-side .news-block');
+      $('.news-block_wrap .right-side').remove();
+      $('.news-block_wrap .left-side .slider-block').insertAfter('.news-block_wrap .left-side'); //выдераем банер-слайдер с новостной колонки и вставляем его после блока. Потому что новости будут иметь свой слайдер
+      $('.news-block_wrap .left-side .slider-block').remove();
 
-      $('.left-side').slick({
+      $('.news-block_wrap .left-side').slick({
           dots: true,
           slidesToShow: 3,
           slidesToScroll: 1,
           infinite: false,
           arrows: false
       });
-  } else {
-   // location.reload();
   }
 };
 
-/***Работа с слайдером Фейсбука***/
+/*Скрипт, который на главной странице вырезает всех партнеров и бросает их в один контейнер,
+поскольку в изначальной реализации подразумевается два блока, а в мобильной версии один*/
+var mainPartnersAdaptive = function () {
+    if(isDisplayTablet()) {
+        var partnersLogos = $('.partners-section_wrap .partners-block_wrap img');
+        partnersLogos.insertBefore('.partners-block_wrap .partners-line:first-child');
+        partnersLogos.wrap('<div class="partner-wrap"></div>')
+        $('.partners-section_wrap .partners-line').remove();
+    }
+};
+/*Пересобираем слайдеры*/
+var tabletSliders = function(){
+    if(isDisplayTablet){
+        /*Слайдер особого мнения*/
+        $('.opinion-slider_block').slick('unslick');
+        $('.opinion-slider_block').slick({
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        fade: false,
+                        arrows: false,
+                        dots: false,
+                        infinite: false,
+                        //centerMode: true,
+                        variableWidth: true
+                    }
+                }
+            ]
+        });
+        //$('.opinion-slider_block').slick('unslick');
+        /*/особое мение*/
+
+        /*сейчас обсуждают*/
+        $('.discussion-block_wrap').slick({
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        fade: false,
+                        arrows: false,
+                        dots: true,
+                        infinite: true,
+                        //centerMode: true,
+                        variableWidth: true
+                    }
+                }
+            ]
+        });
+
+        /*Пресса о нас*/
+        $('.about-section_wrap .events-block_wrap .events-list').slick({
+            dots:true,
+            variableWidth: true,
+            arrows: false
+        });
+
+
+    }
+}
+/*Скрипт который перегружает страницу когда при ресайз проходиться контрольная точка (планшет или телефон)*/
+
+var reloadWhenChangeViews = function(){
+    var tablet = 980;
+    var actualWindowsWidth = $(window).width();
+    if(
+        (startWindowsWidth>tablet)&&(actualWindowsWidth<=tablet)||
+        (startWindowsWidth<=tablet)&&(actualWindowsWidth>tablet)
+    ){
+        location.reload();
+    }
+
+}
